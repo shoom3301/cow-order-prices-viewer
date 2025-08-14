a52181c46f2c347844f1af233af1be405ee6e826
shoom3301 <shoom3301@gmail.com>
2025-08-14 17:51:36 +0500

    chore: fix crash

--- src/logic/getOrderBuyAmountAfterFees.ts
+++ src/logic/getOrderBuyAmountAfterFees.ts
@@ -4,7 +4,7 @@ const ONE_HUNDRED_BPS = (100 * 100)
 
 export function getOrderBuyAmountAfterFees(order: FullOrder): bigint {
     const appData = JSON.parse(order.fullAppData || '{}')
-    const slippagePercentBps: number = appData.metadata.quote.slippageBips ?? 0
+    const slippagePercentBps: number = appData?.metadata?.quote?.slippageBips ?? 0
 
     return BigInt(Math.ceil(+order.buyAmount * (1 + (slippagePercentBps / ONE_HUNDRED_BPS))))
 }
