95b53f540d8c95252e48bb0d2f7a318c0ffd3c5f
shoom3301 <shoom3301@gmail.com>
2026-04-23 13:52:36 +0500

    chore: fix build

--- src/logic/getQuoteAmounts.ts
+++ src/logic/getQuoteAmounts.ts
@@ -42,8 +42,6 @@ export function getQuoteAmounts(order: FullOrder, sellToken: TokenInfo, buyToken
             buyAmount: buyAmount.toString(),
             feeAmount: quoteFeeInSellToken.toString()
         },
-        sellDecimals: sellToken.decimals,
-        buyDecimals: buyToken.decimals,
         slippagePercentBps,
         partnerFeeBps
     })
