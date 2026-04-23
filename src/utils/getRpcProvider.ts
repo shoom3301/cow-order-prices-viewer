e1429c48ed48d2264ca0698d76ce1096bb60289f
shoom3301 <shoom3301@gmail.com>
2026-04-23 13:22:33 +0500

    chore: fix types

--- src/utils/getRpcProvider.ts
+++ src/utils/getRpcProvider.ts
@@ -1,7 +1,10 @@
 import { JsonRpcProvider } from "@ethersproject/providers";
+import type { EvmChainInfo } from "@cowprotocol/sdk-config";
 import { ALL_SUPPORTED_CHAINS_MAP, type SupportedChainId } from "@cowprotocol/cow-sdk";
 import { cacheableByChain } from "./cacheableByChain.ts";
 
 export const getRpcProvider = cacheableByChain((chainId: SupportedChainId) => {
-    return new JsonRpcProvider(ALL_SUPPORTED_CHAINS_MAP[chainId].rpcUrls.default.http[0]);
+    const chain = ALL_SUPPORTED_CHAINS_MAP[chainId] as EvmChainInfo
+
+    return new JsonRpcProvider(chain.rpcUrls.default.http[0]);
 })
